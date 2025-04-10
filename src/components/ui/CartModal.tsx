9e8f76f153cc14a48e1f81d7a42ae1a204793070
Krzysztof <drkrzysztofzieba@gmail.com>
2025-04-10 18:12:45 +0200

    responsive modal window

--- src/components/ui/CartModal.tsx
+++ src/components/ui/CartModal.tsx
@@ -7,7 +7,7 @@ type CartModalPropsType = {
 function CartModal({ children }: CartModalPropsType) {
   return (
     <div className="fixed inset-0 z-20 overflow-y-auto bg-slate-900/50 text-slate-50 backdrop-blur-xs">
-      <div className="relative top-1/2 mx-auto max-h-[90vh] w-10/12 -translate-y-1/2 overflow-auto rounded-md bg-slate-700 p-4">
+      <div className="relative top-1/2 mx-auto max-h-[90vh] w-10/12 -translate-y-1/2 overflow-auto rounded-md bg-slate-700 p-4 sm:w-[34rem]">
         {children}
       </div>
     </div>
